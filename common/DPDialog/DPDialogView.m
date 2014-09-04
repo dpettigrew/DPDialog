944e1faf4334fae1560699e6f7c9958d4bdf6cad
David Pettigrew <dpettigrew@burnsidedigital.com>
2014-09-04 13:49:09 -0700

    handling of device orientation

--- common/DPDialog/DPDialogView.m
+++ common/DPDialog/DPDialogView.m
@@ -22,6 +22,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self registerNotifications];
     }
     return self;
 }
@@ -30,15 +31,23 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         // Initialization code
+        [self registerNotifications];
     }
     return self;
+}
+
+- (void)registerNotifications {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(position:)
+                                                 name:UIApplicationDidChangeStatusBarOrientationNotification
+                                               object:nil];
 }
 
 - (void)show {
     self.windowView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
     [self.windowView addSubview:self];
     [[[[UIApplication sharedApplication] delegate] window] addSubview:self.windowView];
-    [self position];
+    [self position:nil];
 }
 
 - (void)dismiss {
@@ -59,38 +68,63 @@
      self.layer.shadowPath = shadowPath.CGPath;
 }
 
-- (void)position {
+- (void)position:(NSNotification*)notification {
+    CGRect orientationFrame = self.window.bounds;
+    CGRect statusBarFrame = [UIApplication sharedApplication].statusBarFrame;
+    
     UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
     // no transforms applied to window in iOS 8
     BOOL ignoreOrientation = [[NSProcessInfo processInfo] respondsToSelector:@selector(operatingSystemVersion)];
+
+    if(!ignoreOrientation && UIInterfaceOrientationIsLandscape(orientation)) {
+        float temp = orientationFrame.size.width;
+        orientationFrame.size.width = orientationFrame.size.height;
+        orientationFrame.size.height = temp;
+        
+        temp = statusBarFrame.size.width;
+        statusBarFrame.size.width = statusBarFrame.size.height;
+        statusBarFrame.size.height = temp;
+    }
     
+    CGFloat activeHeight = orientationFrame.size.height;
+    CGFloat posY = floor(activeHeight*0.45);
+    CGFloat posX = orientationFrame.size.width/2;
+    
+    CGPoint newCenter;
     CGFloat rotateAngle;
     
     if (ignoreOrientation) {
         rotateAngle = 0.0;
+        newCenter = CGPointMake(posX, posY);
     }
     else {
         switch (orientation) {
             case UIInterfaceOrientationPortraitUpsideDown:
                 rotateAngle = M_PI;
+                newCenter = CGPointMake(posX, orientationFrame.size.height-posY);
                 break;
             case UIInterfaceOrientationLandscapeLeft:
                 rotateAngle = -M_PI/2.0f;
+                newCenter = CGPointMake(posY, posX);
                 break;
             case UIInterfaceOrientationLandscapeRight:
                 rotateAngle = M_PI/2.0f;
+                newCenter = CGPointMake(orientationFrame.size.height-posY, posX);
                 break;
             default: // as UIInterfaceOrientationPortrait
                 rotateAngle = 0.0;
+                newCenter = CGPointMake(posX, posY);
                 break;
         }
     }
     
-    [self applyRotation:rotateAngle];
+    [self moveToPoint:newCenter rotateAngle:rotateAngle];
 }
 
-- (void)applyRotation:(CGFloat)angle {
+- (void)moveToPoint:(CGPoint)newCenter rotateAngle:(CGFloat)angle {
+    self.center = CGPointMake(newCenter.x, newCenter.y);
     self.windowView.transform = CGAffineTransformMakeRotation(angle);
 }
+
 
 @end
